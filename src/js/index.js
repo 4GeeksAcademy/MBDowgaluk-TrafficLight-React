5c5321a532db48aab87c8b352cad82c134f8e6fa
Merlina Barbara Dowgaluk <merlina.dowgaluk@gmail.com>
2023-11-03 08:09:50 +0000

    add semaforo styles

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+const TrafficLight = () =>{
+    /*const [ color, setColor] = useState("red");*/
+
+    return(
+        <div className="text-center">
+            <div className="trafficTop"></div>
+            <div className="container bg-dark traffic">
+                <div className="red-light"></div>
+                <div className="yellow-light"></div>
+                <div className="green-light"></div>
+            </div>
+        </div>
+    )
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
